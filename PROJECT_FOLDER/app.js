b2b6f8df792636666acba1415b4c1878bc3dd510
nugrahalazuardim <nugrahalazuardim@gmail.com>
2026-04-25 22:00:32 +0700

    Debug - Penyesuaian endpoint employee

--- PROJECT_FOLDER/app.js
+++ PROJECT_FOLDER/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const employeeRoutes = require("./app/routes/employee-route");
+console.log("employeeRoutes =", employeeRoutes);
+
 app.use(express.json());
+
+app.use("/api/employees", employeeRoutes);
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+});
 
 module.exports = app;
