4e743f9a98c9f6376f6433cea98750163e357f24
nugrahalazuardim <nugrahalazuardim@gmail.com>
2026-04-27 09:39:16 +0700

    Debug - Penyesuaian wiring model berdasarkan seqeulize format

--- PROJECT_FOLDER/app/models/employee_family-model.js
+++ PROJECT_FOLDER/app/models/employee_family-model.js
@@ -1,68 +1,70 @@
 const { DataTypes, Sequelize } = require("sequelize");
 const { sequelize } = require("../config/db_test");
 
-const EmployeeFamily = sequelize.define("employee_family",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false
-        },
-        employee_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        identifier: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        job: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        place_of_birth: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        date_of_birth: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        religion: DataTypes.ENUM('Islam', 'Katolik', 'Protestan', 'Konghucu'),
-        is_life: DataTypes.BOOLEAN,
-        is_divorced: DataTypes.BOOLEAN,
-        relation_status: DataTypes.ENUM('Suami', 'Istri', 'Anak', 'Anak Sambung'),
-        created_by: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        updated_by: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        updated_at: {
-            type: DataTypes.STRING,
-            allowNull: false
+module.exports = (sequelize, DataTypes) => {
+    const EmployeeFamily = sequelize.define("employee_family",
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                allowNull: false
+            },
+            employee_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            identifier: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            job: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            place_of_birth: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            date_of_birth: {
+                type: DataTypes.DATE,
+                allowNull: true
+            },
+            religion: DataTypes.ENUM('Islam', 'Katolik', 'Protestan', 'Konghucu'),
+            is_life: DataTypes.BOOLEAN,
+            is_divorced: DataTypes.BOOLEAN,
+            relation_status: DataTypes.ENUM('Suami', 'Istri', 'Anak', 'Anak Sambung'),
+            created_by: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            updated_by: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            created_at: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            updated_at: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+        }, {
+            tableName: "employee_family",
+            timestamps: false
         }
-    }, {
-        tableName: "employee_family",
-        timestamps: false
+    );
+
+    EmployeeFamily.associate = (models) => {
+        EmployeeFamily.belongsTo(models.employee, {
+            foreignKey: 'employee_id',
+            as: 'employee'
+        });
     }
-);
 
-EmployeeFamily.associate = (models) => {
-    EmployeeFamily.belongsTo(models.employee, {
-        foreignKey: 'employee_id',
-        as: 'employee'
-    });
+    return EmployeeFamily;
 }
-
-module.exports = EmployeeFamily;
--- PROJECT_FOLDER/app/models/employee_profile-model.js
+++ PROJECT_FOLDER/app/models/employee_profile-model.js
@@ -1,58 +1,60 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/db_test");
 
-const EmployeeProfile = sequelize.define("employee_profile",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false
-        },
-        employee_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        place_of_birth: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        date_of_birth: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        gender: DataTypes.ENUM('Laki-Laki','Perempuan'),
-        is_married: DataTypes.BOOLEAN,
-        prof_pict: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        created_by: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        updated_by: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        updated_at: {
-            type: DataTypes.DATE,
-            allowNull: false
+module.exports = (sequelize, DataTypes) => {
+    const EmployeeProfile = sequelize.define("employee_profile",
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                allowNull: false
+            },
+            employee_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            place_of_birth: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            date_of_birth: {
+                type: DataTypes.DATE,
+                allowNull: true
+            },
+            gender: DataTypes.ENUM('Laki-Laki','Perempuan'),
+            is_married: DataTypes.BOOLEAN,
+            prof_pict: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            created_by: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            updated_by: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            created_at: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            updated_at: {
+                type: DataTypes.DATE,
+                allowNull: false
+            }
+        }, {
+            tableName: "employee_profile",
+            timestamps: false
         }
-    }, {
-        tableName: "employee_profile",
-        timestamps: false
+    );
+
+    EmployeeProfile.associate = (models) => {
+        EmployeeProfile.belongsTo(models.employee, {
+            foreignKey: 'employee_id',
+            as: 'employee'
+        });
     }
-);
 
-EmployeeProfile.associate = (models) => {
-    EmployeeProfile.belongsTo(models.employee, {
-        foreignKey: 'employee_id',
-        as: 'employee'
-    });
+    return EmployeeProfile;
 }
-
-module.exports = EmployeeProfile;
